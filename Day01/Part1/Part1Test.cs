4693719031d35a0061682ada0cc4a0caf220c0d1
Nicholas Lemay <lemaynicholas@gmail.com>
2022-12-07 08:28:07 -0500

    Elves calories is spread over multiple items

--- Day01/Part1/Part1Test.cs
+++ Day01/Part1/Part1Test.cs
@@ -10,14 +10,14 @@ public class Part1Test
     [Fact]
     public void ElvesKnowHighestCaloryCount()
     {
-        Elves.HighestCaloryCount(new List<int>{1,3,2}).Should().Be(3);
+        Elves.HighestCaloryCount(new List<List<int>>{new (){1},new(){3},new(){2}}).Should().Be(3);
     }
 }
 
 public class Elves
 {
-    public static int HighestCaloryCount(List<int> calories)
+    public static int HighestCaloryCount(List<List<int>> calories)
     {
-        return calories.Max();
+        return calories.Select(x=>x.First()).Max();
     }
 }

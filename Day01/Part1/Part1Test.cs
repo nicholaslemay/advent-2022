eeab913ec3361808705e8898ac228a47368ca51b
Nicholas Lemay <lemaynicholas@gmail.com>
2022-12-07 08:21:50 -0500

    Elves can return an hardcoded highest calory count

--- Day01/Part1/Part1Test.cs
+++ Day01/Part1/Part1Test.cs
@@ -8,6 +8,14 @@ public class Part1Test
     [Fact]
     public void Test1()
     {
-        1.Should().Be(1);
+        Elves.HighestCaloryCount().Should().Be(1);
+    }
+}
+
+public class Elves
+{
+    public static int HighestCaloryCount()
+    {
+        return 1;
     }
 }

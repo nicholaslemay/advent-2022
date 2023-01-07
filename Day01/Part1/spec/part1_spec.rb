4bd36fde1c3df182fea985f1373766f832ba458d
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-06 21:27:05 -0500

    Elves know highest calory based on param

--- Day01/Part1/spec/part1_spec.rb
+++ Day01/Part1/spec/part1_spec.rb
@@ -1,12 +1,12 @@
 class Elves
-  def self.with_most_calories
-    145
+  def self.with_most_calories(calory)
+    calory
   end
 end
 
 RSpec.describe "Elves" do
 
   it 'know highest calories count' do
-    expect(Elves.with_most_calories).to eq(145)
+    expect(Elves.with_most_calories(172)).to eq(172)
   end
 end

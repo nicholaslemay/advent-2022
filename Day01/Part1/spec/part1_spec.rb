7473a5e7e037da750ba2a9c2e42effca908425dd
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-06 21:44:57 -0500

    Return n lines from file

--- Day01/Part1/spec/part1_spec.rb
+++ Day01/Part1/spec/part1_spec.rb
@@ -4,9 +4,22 @@ class Elves
   end
 end
 
+class CaloryCounts
+def self.from_file(filename)
+  File.readlines(filename)
+end
+
+end
 RSpec.describe "Elves" do
 
   it 'know highest calories count carried per elf' do
     expect(Elves.with_most_calories([[1,1,1],[2,2,2],[3,3,3]])).to eq(9)
+  end
+end
+
+RSpec.describe "Calory counts" do
+
+  it 'From file' do
+    expect(CaloryCounts.from_file('./Part1/day1Sample.txt').count).to eq(14)
   end
 end

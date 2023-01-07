419d89828eafcc6b7961d1976e421fb905cd1f50
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-07 08:42:32 -0500

    Returns calory count per elf

--- Day01/Part2/spec/part2_spec.rb
+++ Day01/Part2/spec/part2_spec.rb
@@ -1,6 +1,6 @@
 class Elves
   def self.with_most_calories(calories_per_elf)
-    calories_per_elf.map(&:sum).max
+    calories_per_elf.map(&:sum)
   end
 end
 
@@ -14,7 +14,7 @@ end
 RSpec.describe "Elves" do
 
   it 'know highest calories count carried per elf' do
-    expect(Elves.with_most_calories([[1,1,1],[2,2,2],[3,3,3]])).to eq(9)
+    expect(Elves.with_most_calories([[1,1,1],[2,2,2],[3,3,3]])).to eq([3,6,9])
   end
 end
 

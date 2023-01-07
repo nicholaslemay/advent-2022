0b30dcb82b3a446e68ef3087989e50d32ad2e125
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-06 21:30:27 -0500

    Highest calory count is sum of calories carried per elf

--- Day01/Part1/spec/part1_spec.rb
+++ Day01/Part1/spec/part1_spec.rb
@@ -1,12 +1,12 @@
 class Elves
   def self.with_most_calories(calories)
-    calories.max
+    calories.map(&:sum).max
   end
 end
 
 RSpec.describe "Elves" do
 
   it 'know highest calories count' do
-    expect(Elves.with_most_calories([2,4,1,3])).to eq(4)
+    expect(Elves.with_most_calories([[1,1,1],[2,2,2],[3,3,3]])).to eq(9)
   end
 end

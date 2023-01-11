becc6c0e3962618eb7e71a9167fec7c3b3815412
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-10 22:29:11 -0500

    Take only calories from TOP 3 elves into sum

--- Day01/Part2/part2.rkt
+++ Day01/Part2/part2.rkt
@@ -5,13 +5,13 @@
 
 (define (sum x) (apply + x))
 (define (most-calories calories-per-elf)
-  (sum (take (map sum calories-per-elf) 3))
+  (sum (take (sort (map sum calories-per-elf) >) 3))
 )
 
 (describe "Calculating top calories by elves"
 
-    (it "returns sum of all calories carried by 3 elves" (lambda (expect)
-      (expect (most-calories '((10 10 10) (9 9 9) (8 8 8) (1 1 1))) (toEqual (+ 30 27 24)))
+    (it "returns sum of all calories carried by top 3 elves" (lambda (expect)
+      (expect (most-calories '((10 10 10) (1 1 1) (9 9 9) (8 8 8) )) (toEqual (+ 30 27 24)))
     ))
 
 )

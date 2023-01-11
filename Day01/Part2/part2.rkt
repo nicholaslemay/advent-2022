011ee54fe5cfb2c1168f8bba22e8bd1b84229234
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-10 22:38:23 -0500

    Refactoring to make the code closer to the domain language

--- Day01/Part2/part2.rkt
+++ Day01/Part2/part2.rkt
@@ -4,14 +4,17 @@
 (require "../day1-input.rkt")
 
 (define (sum x) (apply + x))
-(define (most-calories calories-per-elf)
-  (sum (take (sort (map sum calories-per-elf) >) 3))
+(define (sums-of x)(map sum x)) 
+(define (top n numbers)(take (sort numbers >) 3))
+
+(define (top-calories calories-per-elf)
+  (sum (top 3 (sums-of calories-per-elf)))
 )
 
 (describe "Calculating top calories by elves"
 
-    (it "returns sum of all calories carried by top 3 elves" (lambda (expect)
-      (expect (most-calories '((10 10 10) (1 1 1) (9 9 9) (8 8 8) )) (toEqual (+ 30 27 24)))
+    (it "returns sum of top 3 total calories carried by different elves" (lambda (expect)
+      (expect (top-calories '((10 10 10) (1 1 1) (9 9 9) (8 8 8) )) (toEqual (+ 30 27 24)))
     ))
 
 )

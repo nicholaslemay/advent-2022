0a792a620562dcf36213f33efad5849961b1497c
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-10 22:27:18 -0500

    Takes only 3 elves calory counts into sum

--- Day01/Part2/part2.rkt
+++ Day01/Part2/part2.rkt
@@ -4,12 +4,14 @@
 (require "../day1-input.rkt")
 
 (define (sum x) (apply + x))
-(define (most-calories calories-per-elf) (sum (map sum calories-per-elf)))
+(define (most-calories calories-per-elf)
+  (sum (take (map sum calories-per-elf) 3))
+)
 
 (describe "Calculating top calories by elves"
 
-    (it "returnssum of all calories carried by elves" (lambda (expect)
-      (expect (most-calories '((1 1 1) (3 3 3) (2 2 2))) (toEqual (+ 3 9 6)))
+    (it "returns sum of all calories carried by 3 elves" (lambda (expect)
+      (expect (most-calories '((10 10 10) (9 9 9) (8 8 8) (1 1 1))) (toEqual (+ 30 27 24)))
     ))
 
 )

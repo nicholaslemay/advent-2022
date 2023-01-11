dd10abe7ec3d67ffb3eae13975ba8bf86fb0fd70
Nicholas Lemay <lemaynicholas@gmail.com>
2023-01-10 22:01:20 -0500

    Introduce sum helper

--- Day01/Part1/part1.rkt
+++ Day01/Part1/part1.rkt
@@ -1,13 +1,18 @@
 #lang racket
 (require "../../schemy-spec.rkt")
 
-
-(define (most-calories calory) (apply max calory))
+(define (sum x) (apply + x))
+(define (most-calories calories) (apply max calories))
 
 (describe "Calculating most calories per elf"
 
     (it "returns highest calory received" (lambda (expect)
       (expect (most-calories '(1 2 3 4 3 2 1)) (toEqual 4))
+    ))
+
+
+    (it "can sum calories per elf" (lambda (expect)
+      (expect (sum '(1 2 3 4)) (toEqual 10))
     ))
 
 )
